305bf2f41b55c4eec7ac0bce29064a335cf49a99
richzjc <zhangjianchuan@wallstreetcn.com>
2020-02-23 22:44:18 +0800

    添加了下载的状态

--- download/src/main/java/com/richzjc/download/Const.kt
+++ download/src/main/java/com/richzjc/download/Const.kt
@@ -1,3 +1,9 @@
 package com.richzjc.download
 
 const val MAX_HOLD_DOWNLOAD_COUNT = 100
+
+const val WAITING = 0
+const val DOWNLOADING = 10
+const val DOWNLOAD_PAUSE = 20
+const val DOWNLOAD_FINISH = 30
+const val DOWNLOAD_ERROR = 40
