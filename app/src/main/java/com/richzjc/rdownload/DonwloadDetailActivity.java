b20c5ea371503bea7270cc867c536d49aae431d7
richzjc <zhangjianchuan@wallstreetcn.com>
2020-03-03 13:42:27 +0800

    完成了下载功能

--- app/src/main/java/com/richzjc/rdownload/DonwloadDetailActivity.java
+++ app/src/main/java/com/richzjc/rdownload/DonwloadDetailActivity.java
@@ -16,11 +16,18 @@ public class DonwloadDetailActivity extends AppCompatActivity {
     TextView downloadCount;
 
     RDownloadClient downloadClient;
-    String[] urls = new String[]{"http://img.tukuppt.com/newpreview_music/08/99/00/5c88d4a8d1f5745026.mp3",
-            "http://img.tukuppt.com/newpreview_music/08/99/00/5c88d4a8d1f5745026.mp3",
-            "http://img.tukuppt.com/newpreview_music/09/00/75/5c894afe4de5f1618.mp3",
+//    String[] urls = new String[]{"http://img.tukuppt.com/newpreview_music/08/99/00/5c88d4a8d1f5745026.mp3",
+//            "http://img.tukuppt.com/newpreview_music/08/99/00/5c88d4a8d1f5745026.mp3",
+//            "http://img.tukuppt.com/newpreview_music/09/00/75/5c894afe4de5f1618.mp3",
+//            "http://img.tukuppt.com/newpreview_music/09/00/32/5c89189c4f4cf81405.mp3",
+//            "http://img.tukuppt.com/newpreview_music/08/98/97/5c88d1231eeb570304.mp3"};
+
+    String[] urls = new String[]{"https://imtt.dd.qq.com/16891/apk/2D67F8F86F94891D637AFC14E2876F70.apk?fsname=com.tencent.tmgp.speedmobile_1.17.0.41206_1170041206.apk",
+            "https://imtt.dd.qq.com/16891/apk/C48A73CCED637EF18A57BAD865A1714F.apk",
+            "https://imtt.dd.qq.com/16891/apk/5AA1A2800257848F3195B2D0F41D5F3E.apk",
             "http://img.tukuppt.com/newpreview_music/09/00/32/5c89189c4f4cf81405.mp3",
             "http://img.tukuppt.com/newpreview_music/08/98/97/5c88d1231eeb570304.mp3"};
+
     @SizeChange
     public void onSizeChange(int size){
         downloadCount.setText("下载队列里面的数量有： " +  size);
