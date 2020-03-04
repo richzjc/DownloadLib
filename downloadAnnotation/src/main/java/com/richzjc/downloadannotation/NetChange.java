a1a2c924f318d68d94ad0e6b0b965290de13fe6b
richzjc <zhangjianchuan@wallstreetcn.com>
2020-03-04 11:29:26 +0800

    添加了两个回调监听

---
+++ downloadAnnotation/src/main/java/com/richzjc/downloadannotation/NetChange.java
@@ -0,0 +1,14 @@
+package com.richzjc.downloadannotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 当所有的下载任务都暂停了 会执行该注解标记的方法
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.METHOD)
+public @interface NetChange {
+}
